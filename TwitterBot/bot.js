39ab1ff890112435e001c63b0ae47c1b498cc0b8
nithinraok <nithinrao.koluguri@gmail.com>
2017-03-29 22:01:37 +0530

    Working Status Update

--- TwitterBot/bot.js
+++ TwitterBot/bot.js
@@ -4,30 +4,55 @@ var Twit = require('twit')
 
 var T = new Twit(config)
 
-var parameters =  { 
-        q: '#IndvsAus', 
-        count: 10,
-        result_type : 'mixed'
-    }
+var stream = T.stream('user');
 
-T.get('search/tweets',parameters,gotData)
+stream.on('follow',followed);
 
-function gotData(err, data, response) {
-    var tweets=data.statuses;
-    for (var i=0;i<tweets.length;i++){
-        console.log(tweets[i].favorite_count);
-        if(tweets[i].favorite_count > 500){
-        console.log(tweets[i].text);
-    }
- }
+function followed(eventMsg){
+    var name = eventMsg.source.name;
+    var screenname= eventMsg.source.screen_name;
+    postThis('@' +screenname + " Its time to sit and Enjoy Cricket #CricketBot" )
 }
 
-var tweet={
-    status : "First Tweet Posted using Bot #CricketBot"
-};
+tweeting();
 
-T.post('statuses/update',tweet,tweeted);
+setInterval(tweeting,1000*60*4);
 
-function tweeted(err,data,response){
-        console.log("It worked");
+function tweeting(){
+    var parameters =  { 
+            q: '#IPL2017 OR #IPL10 OR Cricket', 
+            lang : 'en',
+            count: 1,
+            result_type:'popular'
+        }
+        
+    T.get('search/tweets',parameters,gotData)
+
+    function gotData(err, data, response) {
+        // console.log(data)
+        var tweets=data.statuses;
+        var gotID=[];
+        for (var i=0;i<tweets.length;i++){
+            console.log(tweets[i].text);
+            gotID[i]=tweets[i].id;
+            // console.log("\n"+gotID[i]);
+     }
+     
+        var string=tweets[tweets.length-1].text
+        postThis(string)
+    }
+ }
+
+function postThis(string){
+    console.log("Okay")
+     var tweet={             
+         status : string
+     }
+
+     //T.post('statuses/retweet/:id',tweet,tweeted);
+     T.post('statuses/update',tweet,tweeted);
+     
+     function tweeted(err,data,response){
+             console.log(data);
+     }
 }
