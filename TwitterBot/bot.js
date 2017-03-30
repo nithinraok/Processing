ebda85b67fe0606aa5bde09ddc7c8e3170483399
nithinraok <nithinrao.koluguri@gmail.com>
2017-03-30 20:47:34 +0530

    Changed count to 30 and Deployed

--- TwitterBot/bot.js
+++ TwitterBot/bot.js
@@ -11,48 +11,59 @@ stream.on('follow',followed);
 function followed(eventMsg){
     var name = eventMsg.source.name;
     var screenname= eventMsg.source.screen_name;
-    postThis('@' +screenname + " Its time to sit and Enjoy Cricket #CricketBot" )
+    postThisFollow('@' +screenname + " Its time to sit and Enjoy Cricket #CricketBot" )
 }
 
 tweeting();
 
-setInterval(tweeting,1000*60*4);
+setInterval(tweeting,1000*60*15); //For Every 15 min
 
 function tweeting(){
     var parameters =  { 
-            q: '#IPL2017 OR #IPL10 OR Cricket', 
+            q: '#IPL2017 OR #IPL10 OR Cricket OR #OrangeArmy OR from:virendersehwag', 
             lang : 'en',
-            count: 1,
+            count: 30,
             result_type:'popular'
         }
         
     T.get('search/tweets',parameters,gotData)
 
     function gotData(err, data, response) {
-        // console.log(data)
+
         var tweets=data.statuses;
-        var gotID=[];
-        for (var i=0;i<tweets.length;i++){
+        var gotID;
+        var i = Math.floor(Math.random()*100);
+        i=i%30;
+        //for (var i=0;i<tweets.length;i++){
             console.log(tweets[i].text);
-            gotID[i]=tweets[i].id;
+            gotID=tweets[i].id_str;
             // console.log("\n"+gotID[i]);
-     }
+    // }
      
-        var string=tweets[tweets.length-1].text
-        postThis(string)
+        var string=tweets[i].text
+        postThisID(gotID);
     }
  }
 
-function postThis(string){
-    console.log("Okay")
-     var tweet={             
-         status : string
+function postThisID(string){
+        var tweet={             
+            id : string
+        }   
+         T.post('statuses/retweet/:id',tweet,tweeted);
+              
+     function tweeted(err,data,response){
+             console.log("Retweeted");
      }
+     
+}
 
-     //T.post('statuses/retweet/:id',tweet,tweeted);
-     T.post('statuses/update',tweet,tweeted);
+function postThisFollow(string){
+        var tweet={             
+            status : string
+        }   
+         T.post('statuses/update',tweet,tweeted);
      
      function tweeted(err,data,response){
-             console.log(data);
-     }
+             console.log("Retweeted");
+        }
 }
